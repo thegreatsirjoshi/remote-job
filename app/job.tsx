285d12c64321e4c878a0946b506b2879305f0411
Akhilesh Joshi <akhilesh.joshi@totmtech.in>
2024-12-08 17:19:37 +0530

    first commit

--- app/job.tsx
+++
@@ -1,8 +0,0 @@
-// Define the Job type
-interface Job {
-    id: number;
-    title: string;
-    company: string;
-    location: string;
-    description: string; // This will contain HTML formatted text
-}
